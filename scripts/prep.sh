fb51613b3e91f74c493f677d1bc023c2b24101b3
zhangying.1998 <zhangying.1998@bytedance.com>
2023-03-03 18:15:12 +0800

    add a script_sh

---
+++ scripts/prep.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/HandH1998/mct_former.git
+cd mct_former
+mkdir datasets
+cd datasets
+
+hdfs dfs -get /home/byte_arnold_hl_mlnlc/user/zhangying.1998/datasets/coco.tar.gz && \
+tar -xzvf coco.tar.gz && \
+rm -rf coco.tar.gz
+
+cd ..
+
+mkdir models
+cd models
+hdfs dfs -get /home/byte_arnold_hl_mlnlc/user/zhangying.1998/models/coco
+cd ..
